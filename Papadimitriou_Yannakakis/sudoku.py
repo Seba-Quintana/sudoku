90405ec5ca4480669edcf51b59a715a95d1fd973
Seba-Quintana <sequra128@gmail.com>
2023-11-19 03:08:46 -0300

    removing unnecesary things

--- Papadimitriou_Yannakakis/sudoku.py
+++ Papadimitriou_Yannakakis/sudoku.py
@@ -4,7 +4,7 @@
 import heapq
 import copy
 
-# sudoku board
+# sudoku example board
 board = [
     [0, 4, 0, 0, 0, 0, 0, 8, 0],
     [0, 0, 7, 0, 0, 0, 0, 6, 0],
@@ -17,6 +17,7 @@ board = [
     [0, 0, 0, 0, 0, 0, 0, 0, 1],
 ]
 
+# test board
 board2 = [
     [5, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -56,35 +57,32 @@ def findMIS(original_board):
     for i in range(len(board)):
         for j in range(len(board)):
             quadrant = (i // 3) * 3 + (j // 3)
-            # found a value
+            # found a value for the first time
             if board[i][j] != 0 and not found_one:
                 found_one = True
-                # find initial's value quadrant
                 initial_value = [i, j, quadrant, board[i][j]]
                 quadrants[i].append(initial_value[3])
                 MIS.append(initial_value)
                 continue
-            # every time we find the value again, we update the quadrant list
             if found_one:
                 if board[i][j] == initial_value[3]:
                     MIS.append([i, j, quadrant, initial_value[3]])
-                    # find initial's value quadrant
                     quadrants[i].append(initial_value[3])
                 if board[i][j] == 0:
                     is_in_quadrant = False
                     is_in_row = False
                     is_in_column = False
 
-                    # check if the position's quadrant contains the value
+                    # check if the actual position's quadrant contains the value
                     quadrant = (i // 3) * 3 + (j // 3)
                     if initial_value[3] in quadrants[quadrant]:
                         is_in_quadrant = True
                     
-                    # check if the position's row contains the value
+                    # check if the actual position's row contains the value
                     if initial_value[3] in board[i]:
                         is_in_row = True
 
-                    # check if the position's column contains the value
+                    # check if the actual position's column contains the value
                     for k in range(len(board)):
                         if initial_value[3] == board[k][j]:
                             is_in_column = True
@@ -102,7 +100,6 @@ def findMIS(original_board):
     print_board(board)
     return MIS
 
-# function to print the board
 def print_board(board):
     # traverse the print_board
     for i in range(len(board)):
@@ -145,30 +142,3 @@ def sudoku(board):
 a = sudoku(board)
 # print_board(new_board)
 
-
-
-
-
-
-
-
-        # foreach j in V(G) adyacent to a vertex i in S with i < j
-    #     for i in range(len(board)):
-    #         for j in range(i+1, len(board)):
-    #             # Check if j is adjacent to i in S
-    #             if board[i][j] == 0 and i in S and j in S:
-    #                 # Compute S_j = S intersection {i, ..., j}
-    #                 S_j = set(range(i, j+1)) & S
-    #
-    #                 # Compute the set difference S_j - neighbors(j) union {j}
-    #                 neighbors_j = {k for k in range(len(board)) if board[j][k] == 0 and k != j}
-    #                 potential_set = (S_j - neighbors_j) | {j}
-    #
-    #                 # Check if potential_set is a maximal independent set in {i, ..., j}
-    #                 if all(board[x][y] == 0 or (x not in potential_set and y not in potential_set) for x in range(i, j+1) for y in range(i, j+1) if x != y):
-    #                     # Find the lex-first maximal independent set containing potential_set
-    #                     T = findMIS(board)
-    #                     # Insert T into the heap Q
-    #                     Q.insert(T)
-    #
-    # # Return the list L of maximal independent sets
