f43532c172642ee48430527b8ce05b1ebbb4bd96
Seba-Quintana <sequra128@gmail.com>
2023-11-20 01:23:10 -0300

    S's for, {1,...,j} set and Sj

--- Papadimitriou_Yannakakis/sudoku.py
+++ Papadimitriou_Yannakakis/sudoku.py
@@ -5,7 +5,7 @@ import heapq
 import copy
 
 # sudoku example board
-board = [
+board1 = [
     [0, 4, 0, 0, 0, 0, 0, 8, 0],
     [0, 0, 7, 0, 0, 0, 0, 6, 0],
     [0, 0, 0, 0, 1, 0, 0, 0, 0],
@@ -95,11 +95,12 @@ def findMIS(original_board):
                     # if the value is neither in the quadrant nor in the row nor in the column
                     # add it to the MIS
                     MIS.append([i, j, quadrant, initial_value[3]])
+                    # board is for testing purposes
                     board[i][j] = initial_value[3]
                     # update quadrant
                     quadrants[quadrant].append(initial_value[3])
         continue
-    print_board(board)
+    # print_board(board)
     return MIS
 
 def print_board(board):
@@ -120,7 +121,7 @@ def print_board(board):
                 print(str(board[i][j]) + " ", end="")
     print("\n")
 
-def get_quadrant_cells(x):
+def get_quadrant_cells(board, x):
     # Get the starting row and column of the quadrant
     start_row = (x[0] // 3) * 3
     start_col = (x[1] // 3) * 3
@@ -129,7 +130,8 @@ def get_quadrant_cells(x):
     quadrant_cells = []
     for i in range(start_row, start_row + 3):
         for j in range(start_col, start_col + 3):
-            quadrant_cells.append((i, j))
+            cell = [i, j, x[2], board[i][j]]
+            quadrant_cells.append(cell)
 
     return quadrant_cells
 
@@ -140,6 +142,7 @@ def sudoku(board):
     Q = []
     heapq.heappush(Q, S_star)
     L = []
+    print_board(board)
     # while the heap is not empty
     while Q:
         # extract the first element of the heap
@@ -148,20 +151,42 @@ def sudoku(board):
         L.append(S)
         # foreach j in V(G) adyacent to a vertex i in S with i < j 
         # for this you can check the position for every vertex in S, and search in the board for the adjacent vertex (vertex with same i, j, or in the same quadrant)
+
+        # k's idea is to store all vertex in S
+        k = copy.deepcopy(S)
         for x in S:
             row = board[x[0]]
             column = []
             for i in range(len(board)):
                 column.append(board[i][x[1]])
             quadrant = []
-            quadrant_cells = get_quadrant_cells(x)
+            quadrant_cells = get_quadrant_cells(board, x)
             for cell in quadrant_cells:
-                cell_value = board[cell[0]][cell[1]]
-                quadrant.append(cell_value)
+                quadrant.append(cell)
 
+            # search for the adjacent vertex
+            Sj = []
+            # j has all adjacent vertex to x
+            j = []
+            actual_row = 0
+            # grab all rows before x[0]
+            while actual_row < x[0]:
+                actual_column = 0
+                for elem in board[actual_row]:
+                    j.append([actual_row, actual_column, (actual_row // 3) * 3 + (actual_column // 3), elem])
+                    actual_column = actual_column + 1
+                actual_row += 1
+            # grab all elements in the same row as x until x[1]
+            actual_column = 0
+            while actual_column <= x[1]:
+                for elem in row:
+                    j.append([x[0], actual_column, x[2], elem])
+                actual_column += 1
+            # grab elements that are in S and j at the same time in Sj
+            # if (Sj - neighbours of j) U {j}
     return L
 
 # new_board = findMIS(board2)
-a = sudoku(board)
+a = sudoku(board1)
 # print_board(new_board)
 
